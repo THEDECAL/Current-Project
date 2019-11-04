7485ade4482672922322df7d207bb20ff670dcae
Никита <thedecal1@gmail.com>
2019-11-04 09:20:14 +0200

    4 ноя 2019 г.  9:20:14

---
+++ OnlinePoker/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using OnlinePoker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlinePoker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var game = new Game() { Players = { new Player(), new Player() } };
+            game.CardDistribution();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ OnlinePoker/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlinePoker.Models
+{
+    public class Game
+    {
+        const int CARDS_FOR_DIST = 5;
+        Deck _deck = new Deck();
+        public List<Player> Players { get; private set; } = new List<Player>();
+        public int Bank { get; set; }
+        public Game()
+        {
+            _deck.Shuffle();
+        }
+        public void CardDistribution()
+        {
+            for (int i = 0; i < CARDS_FOR_DIST; i++)
+                Players.ForEach((p) => p.TakeCard(_deck.GetCard()));
+        }
+    }
+}
